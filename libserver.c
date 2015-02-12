d49ce9411d9cf22dec3a18399ff48a67d3b1c26e
laijia <laijia2008@126.com>
2015-02-12 20:55:03 +0800

    add some files

--- libserver.c
+++ libserver.c
@@ -2,12 +2,54 @@
 int main()
 {
 	lib_write_log(LOGPATH, "the server start...\n");
-	serv serve;
-    	
+	struct st_sysserv sysserv;
+	readconfig(&sysserv);
+	source_init(&sysserv);
+	sysserv.serv.serv_fd = lib_tcpsrv_init(sysserv.server.serv_ip, sysserv.server.serv_port);
+	if(sysserv.serv.serv_fd < 0)
+	{
+		return -1;
+	}
+	lib_set_nonblock(sysserv.server.serv_fd);
+	struct event listen_ev;
+	struct st_dispatch dispatch;
+	dispatch->base = event_base_new();
+    event_set(&listen_ev,sysserv.server.serv_fd,EV_READ|EV_PERSIST, accept_cb, &sysserv);
+	event_base_set(dispatch->base, &listen_ev);
+	event_add(&listen_ev,NULL);
+	event_dispatch(dispatch->base);
+    return 0;
 }
 
-void readconfig(const char *config)
+
+void readconfig(struct st_sysserv *sys_serv)
 {
+	char tmpbuf[50];
+	memset(tmpbuf,0,sizeof(tmpbuf));
+	lib_file_readcfg("server","ip","127.0.0.1",sys_serv->server.serv_ip, 24, CONFFILE);
+    memset(tmpbuf,0, sizeof(tmpbuf));
+	lib_file_readcfg("server","port","9999",tmpbuf, 4, CONFFILE);
+	sys_serv->server.serv_port = atoi(tmpbuf);
+	memset(tmpbuf, 0, sizeof(tmpbuf));
+	lib_file_readcfg("server","thrd_num","10", tmpbuf, 2, CONFFILE);
+	sys_serv->thrd_num = atoi(tmpbuf);
+		
+}
+
+void accept_cb(int fd, short event, void *arg)
+{
+	struct st_client *client;
+    struct st_sysserv *sysserv = (struct st_sysserv*)arg;
+	client->cli_fd = lib_tcpsrv_accept(sysserv->server.serv_fd, &(client->cli_addr));
+	if(client->cli_fd < 0)
+		return -1;
+	else
+		lib_write_log(LOGFILE, "[%s]recv new connection{%s:%d}\n", lib_time_now(), inet_ntoa(client->cli_addr), ntohs(client->cli_addr.port));
+	//保存客户端信息
 	
 }
 
+int resource_init(struct st_server *server)
+{
+	return 0;
+}
--- libserver.h
+++ libserver.h
@@ -5,8 +5,8 @@
 
 #define IPADDR "127.0.0.1"
 #define PORT 3333
-#define LOGPATH "./log/"
-#define CONFPATH "./config/"
+#define LOGFILE "./log/libserver.log"
+#define CONFFILE "./config/libserver.ini"
 typedef struct st_client
 {
 	char name[20];
@@ -14,15 +14,42 @@ typedef struct st_client
 	int cli_fd;
 	pthread_t cli_pid;
 	struct event *cli_event;
-	struct base *cli_base;
-}client;
+	struct event_base *cli_base;	
+};
 
 typedef struct st_server
 {
 	int serv_fd;
-	struct sockaddr_in s
+	char serv_ip[24];
+	int serv_port;
     struct event *serv_event;
-	struct base *serv_base
-}server;
+	struct event_base *serv_base
+};
 
-void readconfig(const char *config, );
+typedef struct st_sysserv
+{
+	struct st_server server;
+    struct st_client_info *client;
+	int thrd_num;
+	
+};
+
+//存储客户端信息
+typedef struct st_client_info
+{
+	int cli_max_num;
+	int cli_info_curnum;
+	struct st_client_info *next;
+	pthread_mutex_t mutex;
+};
+
+
+typedef struct st_dispatch
+{
+	struct event_base *base;
+};
+
+void readconfig(struct st_system *system);
+void timeout_cb(int fd, short event, void *arg);
+void accept_cb(int fd, short event, void *arg);
+
